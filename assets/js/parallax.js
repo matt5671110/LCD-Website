48e6b82eada9b2df10c1f5b4a35938c32f2d68e5
matt5671110 <matthew7195@gmail.com>
2014-05-03 22:28:17 -0600

    Lots of style changes and more content added

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,23 +1,23 @@
 $(document).ready(function () {
     $window = $(window);
-    
+
     $('section[data-type="background"]').each(function(){
      // declare the variable to affect the defined data-type
      var $scroll = $(this);
-                     
-      $(window).scroll(function() {                         
-        var yPos = -($window.scrollTop() / $scroll.data('speed')); 
-         
+
+      $(window).scroll(function() {
+        var yPos = -($window.scrollTop() / $scroll.data('speed'));
+
         // background position
         var coords = '50% '+ yPos + 'px';
- 
+
         // move the background
-        $scroll.css({ backgroundPosition: coords });    
+        $scroll.css({ backgroundPosition: coords });
       }); // end window scroll
    });  // end section function
 
   //Might as well use this to init my popovers as well :D
   $popElems = $(".hasPop"); //cache elements with popovers
   $popElems.popover({ trigger: "click", html: true });
-  $popElems.find("a").click(function (evt) {$(".hasPop").popover('hide');});
+  $(".navbar").find("a").click(function (evt) {$popElems.popover('hide');});
 });
